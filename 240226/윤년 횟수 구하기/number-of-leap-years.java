f3312c4b37d4c82e07b3f5716cc316c3857e09a9
YunGeol Jung <61873563+jhon-rjf@users.noreply.github.com>
2024-02-26 11:10:06 +0900

    Title: 윤년 횟수 구하기, Time: 139ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240226/윤년 횟수 구하기/number-of-leap-years.java
+++ 240226/윤년 횟수 구하기/number-of-leap-years.java
@@ -5,9 +5,12 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int year = sc.nextInt();
         int yun_year = 0;
-        for(int i = 0; i <year; i++){
-            if(i%4==0 && i>1 && i%400!=0){
+        for(int i = 1; i <year; i++){
+            if(i%4==0){
                 yun_year++;
+                if(i%100==0 && i%400!=0){
+                    yun_year--;
+                }
             }
         }
         System.out.print(yun_year);

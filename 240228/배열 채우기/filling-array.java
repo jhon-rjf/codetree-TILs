964a0d87ed4600ef54fa0cdf76764c0215fc7908
YunGeol Jung <61873563+jhon-rjf@users.noreply.github.com>
2024-02-28 14:35:18 +0900

    Title: 배열 채우기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240228/배열 채우기/filling-array.java
+++ 240228/배열 채우기/filling-array.java
@@ -4,15 +4,17 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int arr[] = new int[10];
-        for(int i = 0;  i<10; i++){
-            arr[i]=sc.nextInt();
+        int i = 0;
+        for(;  i<10; i++){
+            arr[i]=sc.nextInt();   
             if(arr[i]==0){
-                for(int z=i-1; z>=0; z--){
-                    System.out.print(arr[z]+" ");
-                }
                 break;
-            }
-            
+                
+            }   
+        }
+        i --;
+        for(; i>=0; i--){
+            System.out.print(arr[i]+" ");
         }
     }
 }

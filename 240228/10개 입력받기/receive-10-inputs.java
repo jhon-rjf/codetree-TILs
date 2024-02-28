f9eaae94594878c95e1e1a184235d6386951ef9f
YunGeol Jung <61873563+jhon-rjf@users.noreply.github.com>
2024-02-28 14:49:33 +0900

    Title: 10개 입력받기, Time: 159ms, Memory: 10MB, Status: Passed - CodeTree

--- 240228/10개 입력받기/receive-10-inputs.java
+++ 240228/10개 입력받기/receive-10-inputs.java
@@ -4,18 +4,21 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int arr[] = new int[10];
-        int i = 0;
+        
+        double count = 0;
         int sum = 0;
-        for(;  i<10; i++){
+        for(int i = 0;  i<10; i++){
             arr[i]=sc.nextInt();   
             sum += arr[i];
+            count++;
             if(arr[i]==0){
                 sum -= arr[i];
+                count --;
                 break;
             }   
         }
-        
-        double avg = sum/i;
+    
+        double avg = sum/count;
         System.out.print(sum+" ");
         System.out.printf("%.1f", avg);
         

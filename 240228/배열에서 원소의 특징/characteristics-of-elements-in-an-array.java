0a199c5f9546a87c0ed9ff2195fa37fb5ddda9bc
YunGeol Jung <61873563+jhon-rjf@users.noreply.github.com>
2024-02-28 16:24:44 +0900

    Title: 배열에서 원소의 특징, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240228/배열에서 원소의 특징/characteristics-of-elements-in-an-array.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[10];
+        int i = 0;
+        for(;i<10; i++){
+            arr[i]=sc.nextInt();
+            if(arr[i]%3==0){
+                break;
+            }
+        }
+        System.out.print(arr[i-1]);
+    }
+}

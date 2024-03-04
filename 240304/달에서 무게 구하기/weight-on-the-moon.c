f07e2de9fe9c7e9063fce61f49bb0661711e1718
YunGeol Jung <61873563+jhon-rjf@users.noreply.github.com>
2024-03-04 21:49:49 +0900

    Title: 달에서 무게 구하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240304/달에서 무게 구하기/weight-on-the-moon.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int weight = 13;
+    double gravityOfMoon = 0.165;
+    printf("%d * %.6lf = %.6lf",weight,gravityOfMoon,weight*gravityOfMoon);
+    return 0;
+}
